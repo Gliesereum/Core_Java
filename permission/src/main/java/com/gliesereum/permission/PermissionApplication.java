02ad7b4986116e8f1c619ffed5641bf7e243f1c3
Yurii Vlasiuk <yvlasiuk@gliesereum.com>
2018-12-04 16:02:32 +0200

    add logging to permission fix dockerfiles

--- permission/src/main/java/com/gliesereum/permission/PermissionApplication.java
+++ permission/src/main/java/com/gliesereum/permission/PermissionApplication.java
@@ -3,12 +3,14 @@ package com.gliesereum.permission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
         "com.gliesereum.permission",
         "com.gliesereum.share.common.exception.handler",
         "com.gliesereum.share.common.security.jwt"})
+@EnableAsync
 @EnableEurekaClient
 @EnableScheduling
 public class PermissionApplication {
