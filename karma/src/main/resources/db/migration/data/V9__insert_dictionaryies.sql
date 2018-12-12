b5c2583b1769e43e5b8246338f2bdce23cb08856
vitalij <vitalij.gorbunov@gliesereum.com>
2018-12-12 17:09:29 +0200

    add service insert

---
+++ karma/src/main/resources/db/migration/data/V9__insert_dictionaryies.sql
@@ -0,0 +1,15 @@
+INSERT INTO karma.service_class_car (name, description) VALUES ('Стандарт', 'Стандарт');
+INSERT INTO karma.service_class_car (name, description) VALUES ('Эконом', 'Эконом');
+INSERT INTO karma.service_class_car (name, description) VALUES ('Бизнес', 'Бизнес');
+INSERT INTO karma.service_class_car (name, description) VALUES ('Премиум', 'Премиум');
+
+INSERT INTO karma.service (name, description, type) VALUES ('Мойка двигателя', 'Мойка двигателя и моторного отсека, продувка', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Уборка салона', 'Чистка салона пылесосом и влажная уборка пластмассовых деталей', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Ополаскивание', 'Ополаскивание', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Чистка багажника', 'Чистка багажника (пылесос и влажная уборка)', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Полировка кузова', 'Покрытие кузова воском на основе тефлоновой полировки', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Химчистка салона', 'Химчистка салона, полировка пластмассовых деталей салона химическими средствами', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Чистка колесных дисков', 'Чистка колесных дисков', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Бесконтактная мойка', 'Бесконтактная мойка кузова автомобиля, коврики пороги', 'CAR_WASH');
+INSERT INTO karma.service (name, description, type) VALUES ('Ручная мойка', 'Ручная мойка кузова автомобиля, коврики пороги', 'CAR_WASH');
+
