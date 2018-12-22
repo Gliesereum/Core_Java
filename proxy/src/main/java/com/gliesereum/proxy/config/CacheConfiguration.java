29564e47e6769aff53ad2d18b999dea94aabb3a2
Yurii Vlasiuk <yvlasiuk@gliesereum.com>
2018-12-22 14:58:13 +0200

    fix cache

--- proxy/src/main/java/com/gliesereum/proxy/config/CacheConfiguration.java
+++ proxy/src/main/java/com/gliesereum/proxy/config/CacheConfiguration.java
@@ -1,11 +1,20 @@
 package com.gliesereum.proxy.config;
 
-import org.cache2k.extra.spring.SpringCache2kCacheManager;
+import com.gliesereum.share.common.model.dto.account.auth.AuthDto;
+import com.gliesereum.share.common.model.dto.permission.group.GroupDto;
+import org.cache2k.Cache2kBuilder;
+import org.cache2k.jcache.ExtendedMutableConfiguration;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.jcache.JCacheCache;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.cache.Caching;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -19,13 +28,42 @@ import java.util.concurrent.TimeUnit;
 public class CacheConfiguration {
 
     @Bean
-    public CacheManager cacheManager() {
-        return new SpringCache2kCacheManager()
-                .defaultSetup(b -> b.permitNullValues(false))
-                .addCaches(
-                        b -> b.name("group").expireAfterWrite(1, TimeUnit.MINUTES).entryCapacity(1000),
-                        b -> b.name("user-group").expireAfterWrite(1, TimeUnit.MINUTES).entryCapacity(10000),
-                        b -> b.name("tokenInfo").expireAfterWrite(1, TimeUnit.MINUTES).entryCapacity(10000)
-                );
+    public CacheManager cacheManager(javax.cache.CacheManager jCacheManager) {
+        return new JCacheCacheManager(jCacheManager);
+    }
+
+    @Bean
+    public javax.cache.CacheManager jCacheManager() {
+        return Caching.getCachingProvider().getCacheManager();
+    }
+
+    @Bean
+    public Cache groupCache(javax.cache.CacheManager jCacheManager) {
+        javax.cache.Cache groupCache = jCacheManager.createCache("group",
+                ExtendedMutableConfiguration.of(new Cache2kBuilder<UUID, Map>(){}
+                        .entryCapacity(1000)
+                        .expireAfterWrite(1, TimeUnit.MINUTES)));
+        return new JCacheCache(groupCache);
+
+    }
+
+    @Bean
+    public Cache userGroupCache(javax.cache.CacheManager jCacheManager) {
+        javax.cache.Cache groupCache = jCacheManager.createCache("userGroup",
+                ExtendedMutableConfiguration.of(new Cache2kBuilder<String, GroupDto>(){}
+                        .entryCapacity(10000)
+                        .expireAfterWrite(1, TimeUnit.MINUTES)));
+        return new JCacheCache(groupCache);
+
+    }
+
+    @Bean
+    public Cache tokenInfoCache(javax.cache.CacheManager jCacheManager) {
+        javax.cache.Cache groupCache = jCacheManager.createCache("tokenInfo",
+                ExtendedMutableConfiguration.of(new Cache2kBuilder<String, AuthDto>(){}
+                        .entryCapacity(10000)
+                        .expireAfterWrite(1, TimeUnit.MINUTES)));
+        return new JCacheCache(groupCache);
+
     }
 }
