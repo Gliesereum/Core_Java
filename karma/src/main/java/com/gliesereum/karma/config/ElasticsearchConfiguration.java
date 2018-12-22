589515fd721c5e932893627ab0da52627e2d37d0
Yurii Vlasiuk <yvlasiuk@gliesereum.com>
2018-12-22 16:32:15 +0200

    add custom error mapper for elasticsearch

--- karma/src/main/java/com/gliesereum/karma/config/ElasticsearchConfiguration.java
+++ karma/src/main/java/com/gliesereum/karma/config/ElasticsearchConfiguration.java
@@ -1,24 +1,19 @@
 package com.gliesereum.karma.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.vanroy.springdata.jest.JestElasticsearchTemplate;
 import com.github.vanroy.springdata.jest.mapper.DefaultJestResultsMapper;
 import com.github.vanroy.springdata.jest.mapper.JestResultsMapper;
+import com.gliesereum.karma.config.es.CustomEntityMapper;
+import com.gliesereum.karma.config.es.CustomErrorMapper;
 import io.searchbox.client.JestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
-import org.springframework.data.elasticsearch.core.geo.CustomGeoModule;
 import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.mapping.context.MappingContext;
-
-import java.io.IOException;
 
 /**
  * @author yvlasiuk
@@ -32,7 +27,7 @@ public class ElasticsearchConfiguration {
 
     @Bean
     public ElasticsearchOperations elasticsearchTemplate(JestClient jestClient, ElasticsearchConverter elasticsearchConverter, JestResultsMapper jestResultsMapper) {
-        return new JestElasticsearchTemplate(jestClient, elasticsearchConverter, jestResultsMapper);
+        return new JestElasticsearchTemplate(jestClient, elasticsearchConverter, jestResultsMapper, new CustomErrorMapper(), null);
     }
 
     @Bean
@@ -48,25 +43,6 @@ public class ElasticsearchConfiguration {
     @Bean
     public JestResultsMapper jestResultsMapper(SimpleElasticsearchMappingContext mappingContext, ObjectMapper objectMapper) {
         return new DefaultJestResultsMapper(mappingContext, new CustomEntityMapper(objectMapper));
-    }
-
-    public static class CustomEntityMapper implements EntityMapper {
-        private final ObjectMapper objectMapper;
-
-        public CustomEntityMapper(ObjectMapper objectMapper) {
-            this.objectMapper = objectMapper;
-
-        }
-
-        @Override
-        public String mapToString(Object object) throws IOException {
-            return objectMapper.writeValueAsString(object);
-        }
-
-        @Override
-        public <T> T mapToObject(String source, Class<T> clazz) throws IOException {
-            return objectMapper.readValue(source, clazz);
-        }
     }
 
 }
