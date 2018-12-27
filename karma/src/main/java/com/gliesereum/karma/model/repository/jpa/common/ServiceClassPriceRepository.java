4cbdf2a11271d4e97ab54dc3e8cd62452b5a1f8a
Yurii Vlasiuk <yvlasiuk@gliesereum.com>
2018-12-27 15:06:58 +0200

    some fix in service class

--- karma/src/main/java/com/gliesereum/karma/model/repository/jpa/common/ServiceClassPriceRepository.java
+++ karma/src/main/java/com/gliesereum/karma/model/repository/jpa/common/ServiceClassPriceRepository.java
@@ -4,6 +4,7 @@ import com.gliesereum.karma.model.entity.common.ServiceClassPriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -14,4 +15,6 @@ import java.util.UUID;
 public interface ServiceClassPriceRepository extends JpaRepository<ServiceClassPriceEntity, UUID> {
 
     List<ServiceClassPriceEntity> findAllByPriceId(UUID priceId);
+
+    Optional<ServiceClassPriceEntity> findByPriceIdAndServiceClassId(UUID priceId, UUID serviceClassId);
 }
