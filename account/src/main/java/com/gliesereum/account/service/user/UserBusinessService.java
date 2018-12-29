4d8cf772fb35910eae187c311005059af56f0ea9
vitalij <vitalij.gorbunov@gliesereum.com>
2018-12-29 12:44:34 +0200

    fix user to business

--- account/src/main/java/com/gliesereum/account/service/user/UserBusinessService.java
+++ account/src/main/java/com/gliesereum/account/service/user/UserBusinessService.java
@@ -4,9 +4,13 @@ import com.gliesereum.account.model.entity.UserBusinessEntity;
 import com.gliesereum.share.common.model.dto.account.user.UserBusinessDto;
 import com.gliesereum.share.common.service.DefaultService;
 
+import java.util.UUID;
+
 /**
  * @author vitalij
  * @since 12/4/18
  */
 public interface UserBusinessService extends DefaultService<UserBusinessDto, UserBusinessEntity> {
+
+    UserBusinessDto getByUserIdAndBusinessId(UUID userId, UUID businessId);
 }
