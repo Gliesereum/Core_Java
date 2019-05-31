d46cbbb820a08579a379c3ccacc9f0959f57473f
yvlasiuk <yvlasiuk@gliesereum.com>
2019-05-31 11:53:17 +0300

    notification: add create-update date to device and subscribe model

--- notification/src/main/java/com/gliesereum/notification/model/entity/device/UserDeviceEntity.java
+++ notification/src/main/java/com/gliesereum/notification/model/entity/device/UserDeviceEntity.java
@@ -4,10 +4,15 @@ import com.gliesereum.share.common.model.entity.DefaultEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -20,6 +25,7 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "user_device")
+@EntityListeners(AuditingEntityListener.class)
 public class UserDeviceEntity extends DefaultEntity {
 
 	@Column(name = "user_id")
@@ -30,5 +36,13 @@ public class UserDeviceEntity extends DefaultEntity {
 
 	@Column(name = "notification_enable")
 	private Boolean notificationEnable;
+
+	@Column(name = "create_date")
+	@CreatedDate
+	private LocalDateTime createDate;
+
+	@Column(name = "update_date")
+	@LastModifiedDate
+	private LocalDateTime updateDate;
 
 }
--- share/src/main/java/com/gliesereum/share/common/model/dto/notification/device/UserDeviceDto.java
+++ share/src/main/java/com/gliesereum/share/common/model/dto/notification/device/UserDeviceDto.java
@@ -1,10 +1,15 @@
 package com.gliesereum.share.common.model.dto.notification.device;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.gliesereum.share.common.databind.json.LocalDateTimeJsonDeserializer;
+import com.gliesereum.share.common.databind.json.LocalDateTimeJsonSerializer;
 import com.gliesereum.share.common.model.dto.DefaultDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -22,4 +27,12 @@ public class UserDeviceDto extends DefaultDto {
     private String firebaseRegistrationToken;
 
     private Boolean notificationEnable;
+
+    @JsonDeserialize(using = LocalDateTimeJsonDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeJsonSerializer.class)
+    private LocalDateTime createDate;
+
+    @JsonDeserialize(using = LocalDateTimeJsonDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeJsonSerializer.class)
+    private LocalDateTime updateDate;
 }
