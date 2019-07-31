ff5269c20aac4e8e5f9159c8edfe2c6346cfbc0b
forestyura <forestyura@gmail.com>
2019-07-31 11:06:35 +0300

    fix converter

--- share/src/main/java/com/gliesereum/share/common/converter/imp/DefaultConverterImp.java
+++ share/src/main/java/com/gliesereum/share/common/converter/imp/DefaultConverterImp.java
@@ -59,11 +59,9 @@ public class DefaultConverterImp implements DefaultConverter {
     @Override
     public <E, T> Page<T> convert(Page<E> page, Class<T> resultClass) {
         Page<T> result = null;
-        if (page != null) {
-            if (CollectionUtils.isNotEmpty(page.getContent())) {
-                List<T> content = convert(page.getContent(), resultClass);
-                result = new PageImpl<>(content, page.getPageable(), page.getTotalElements());
-            }
+        if ((page != null) && CollectionUtils.isNotEmpty(page.getContent())) {
+            List<T> content = convert(page.getContent(), resultClass);
+            result = new PageImpl<>(content, page.getPageable(), page.getTotalElements());
         }
         return result;
     }
