dc51a5b80ecfe5357a60b94e2a493c757d691831
vitalijgorbunov <vitalij.gorbunov@yahoo.com>
2019-08-15 11:52:52 +0300

    add config in payment

--- payment/src/main/java/com/gliesereum/payment/config/BeanConfiguration.java
+++ payment/src/main/java/com/gliesereum/payment/config/BeanConfiguration.java
@@ -2,25 +2,16 @@ package com.gliesereum.payment.config;
 
 import com.gliesereum.share.common.converter.DefaultConverter;
 import com.gliesereum.share.common.converter.imp.DefaultConverterImp;
-import com.gliesereum.share.common.exception.handler.RestTemplateErrorHandler;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author vitalij
  */
 @Configuration
 public class BeanConfiguration {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new RestTemplateErrorHandler());
-        return restTemplate;
-    }
 
     @Bean
     public ModelMapper modelMapper() {
