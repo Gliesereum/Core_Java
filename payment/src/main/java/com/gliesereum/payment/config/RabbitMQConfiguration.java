6661270b838f70cc982520bb8a0388a7b40c5971
forestyura <forestyura@gmail.com>
2019-08-15 12:00:46 +0300

    update rabbitConfig

--- payment/src/main/java/com/gliesereum/payment/config/RabbitMQConfiguration.java
+++ payment/src/main/java/com/gliesereum/payment/config/RabbitMQConfiguration.java
@@ -1,18 +1,19 @@
 package com.gliesereum.payment.config;
 
+import com.gliesereum.share.common.config.rabbitmq.RabbitMQDefaultConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author vitalij
  */
 @Slf4j
-@EnableRabbit
 @Configuration
+@ComponentScan(basePackageClasses = RabbitMQDefaultConfiguration.class)
 public class RabbitMQConfiguration {
 
     @Bean
