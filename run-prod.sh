5a5017bc175b81e766dd9649fa9d519817ad32e5
forestyura <forestyura@gmail.com>
2019-08-16 14:52:47 +0300

    create version 0.1.0. add production env

--- run-prod.sh
+++ run-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROJECT_HOME="/usr/gliesereum"
+PROJECT_HOME="/usr/coupler"
 cd $PROJECT_HOME
 pwd
 whoami
@@ -16,11 +16,10 @@ sudo gradle clean build -b=permission/build.gradle --no-daemon
 sudo gradle clean build -b=karma/build.gradle --no-daemon
 sudo gradle clean build -b=file/build.gradle --no-daemon
 sudo gradle clean build -b=lending-gallery/build.gradle --no-daemon
-#sudo gradle clean build -b=socket/build.gradle --no-daemon
 sudo gradle clean build -b=notification/build.gradle --no-daemon
 
 echo 'Docker stop containers'       
-docker stack rm gls                 
+docker stack rm couplerServices
 sleep 30                            
 docker rm $(docker ps -a -q) --force
 
@@ -33,22 +32,21 @@ docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-permi
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-karma')     
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-file')
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-lending-gallery')
-#docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-socket')
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-notification')
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'gls-curator')
 
 echo 'Docker build images'
-sudo docker build -t gls-discovery:0.1.0 -f docker/discovery/Dockerfile  ./discovery/build/libs/
-sudo docker build -t gls-account:0.1.0 -f docker/account/Dockerfile  ./account/build/libs/
-sudo docker build -t gls-proxy:0.1.0  -f docker/proxy/Dockerfile  ./proxy/build/libs/
-sudo docker build -t gls-mail:0.1.0 -f docker/mail/Dockerfile  ./mail/build/libs/
-sudo docker build -t gls-permission:0.1.0 -f docker/permission/Dockerfile  ./permission/build/libs/
-sudo docker build -t gls-karma:0.1.0 -f docker/karma/Dockerfile  ./karma/build/libs/
-sudo docker build -t gls-file:0.1.0 -f docker/file/Dockerfile  ./file/build/libs/
-sudo docker build -t gls-lending-gallery:0.1.0 -f docker/lending-gallery/Dockerfile  ./lending-gallery/build/libs/
-sudo docker build -t gls-notification:0.1.0 -f docker/notification/Dockerfile  ./notification/build/libs/
-sudo docker build -t gls-curator:0.1.0 -f docker/curator/Dockerfile  ./config/elk/
+sudo docker build --rm -t gls-discovery:0.1.0 -f docker/discovery/Dockerfile  ./discovery/build/libs/
+sudo docker build --rm -t gls-account:0.1.0 -f docker/account/Dockerfile  ./account/build/libs/
+sudo docker build --rm  -t gls-proxy:0.1.0  -f docker/proxy/Dockerfile  ./proxy/build/libs/
+sudo docker build --rm  -t gls-mail:0.1.0 -f docker/mail/Dockerfile  ./mail/build/libs/
+sudo docker build --rm  -t gls-permission:0.1.0 -f docker/permission/Dockerfile  ./permission/build/libs/
+sudo docker build --rm  -t gls-karma:0.1.0 -f docker/karma/Dockerfile  ./karma/build/libs/
+sudo docker build --rm  -t gls-file:0.1.0 -f docker/file/Dockerfile  ./file/build/libs/
+sudo docker build --rm  -t gls-lending-gallery:0.1.0 -f docker/lending-gallery/Dockerfile  ./lending-gallery/build/libs/
+sudo docker build --rm  -t gls-notification:0.1.0 -f docker/notification/Dockerfile  ./notification/build/libs/
+sudo docker build --rm  -t gls-curator:0.1.0 -f docker/curator/Dockerfile  ./config/elk/
 
 echo 'Docker deploy'                            
-docker stack deploy -c docker/docker-compose-dev-log-demo.yml gls
+docker stack deploy -c docker/docker-compose-prod-log.yml couplerServices
 docker ps 
