bc13b413b448ac49f6aa05f316bc05f786afd404
forestyura <forestyura@gmail.com>
2019-08-29 19:19:31 +0300

    rename faq to information

---
+++ karma/src/main/java/com/gliesereum/karma/controller/information/InformationController.java
@@ -0,0 +1,55 @@
+package com.gliesereum.karma.controller.information;
+
+import com.gliesereum.karma.service.information.InformationService;
+import com.gliesereum.share.common.model.dto.karma.information.InformationDto;
+import com.gliesereum.share.common.model.response.MapResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author yvlasiuk
+ * @version 1.0
+ */
+
+@RestController
+@RequestMapping("/information")
+public class InformationController {
+
+    @Autowired
+    private InformationService informationService;
+
+    @GetMapping
+    public List<InformationDto> getAll(@RequestParam(required = false, name = "tag") String tag) {
+        if (StringUtils.isNotBlank(tag)) {
+            return informationService.getByTag(tag);
+        } else {
+            return informationService.getAll();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public InformationDto getById(@PathVariable("id") UUID id) {
+        return informationService.getById(id);
+    }
+
+    @PostMapping
+    public InformationDto create(@Valid @RequestBody InformationDto dto) {
+        return informationService.create(dto);
+    }
+
+    @PutMapping
+    public InformationDto update(@Valid @RequestBody InformationDto dto) {
+        return informationService.update(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public MapResponse delete(@PathVariable("id") UUID id) {
+        informationService.delete(id);
+        return new MapResponse("true");
+    }
+}
