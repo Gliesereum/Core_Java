dcfd73cb9f8f6a1afe65b7192e61dfd4965db1d4
forestyura <forestyura@gmail.com>
2019-09-12 16:01:20 +0300

    repo

--- karma/src/main/java/com/gliesereum/karma/model/repository/jpa/client/ClientRepository.java
+++ karma/src/main/java/com/gliesereum/karma/model/repository/jpa/client/ClientRepository.java
@@ -1,8 +1,11 @@
 package com.gliesereum.karma.model.repository.jpa.client;
 
+import ch.qos.logback.core.net.server.Client;
 import com.gliesereum.karma.model.entity.client.ClientEntity;
 import com.gliesereum.share.common.model.enumerated.ObjectState;
 import com.gliesereum.share.common.repository.AuditableRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,7 +16,11 @@ import java.util.UUID;
  */
 public interface ClientRepository extends AuditableRepository<ClientEntity> {
 
-    List<ClientEntity> findAllByBusinessIdsInAndObjectState(List<UUID> businessIds, ObjectState objectState);
+    @Query("SELECT c FROM ClientEntity c JOIN c.businessIds b WHERE (b IN :businessIds) AND c.objectState = :objectState")
+    List<ClientEntity> findAllByBusinessIdsInAndObjectState(@Param("businessIds") List<UUID> businessIds,
+                                                            @Param("objectState") ObjectState objectState);
 
-
+    @Query("SELECT c FROM ClientEntity c JOIN c.corporationIds co WHERE (co IN :corporationIds) AND c.objectState = :objectSate")
+    List<Client> findAllByCorporationIdsAndObjectState(@Param("corporationIds") List<UUID> corporationIds,
+                                                       @Param("objectState") ObjectState objectState);
 }
