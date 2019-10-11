9c73a3b52a16e8832ddd3fc1a633737295693c84
forestyura <forestyura@gmail.com>
2019-10-11 12:15:02 +0300

    add new method to clientFacade

--- karma/src/main/java/com/gliesereum/karma/facade/client/ClientFacade.java
+++ karma/src/main/java/com/gliesereum/karma/facade/client/ClientFacade.java
@@ -2,6 +2,7 @@ package com.gliesereum.karma.facade.client;
 
 import com.gliesereum.share.common.model.dto.account.user.PublicUserDto;
 import com.gliesereum.share.common.model.dto.account.user.UserDto;
+import com.gliesereum.share.common.model.dto.karma.client.ClientDto;
 
 import java.util.UUID;
 
@@ -14,6 +15,8 @@ public interface ClientFacade {
     void addNewClient(PublicUserDto user, UUID businessId);
     
     void addNewClient(UserDto user, UUID businessId);
+    
+    ClientDto addNewClientAddGet(PublicUserDto user, UUID businessId);
     
     void updateClientInfo(UserDto user);
     
